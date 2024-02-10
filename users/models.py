ad144d5524ddff92c638eb1fb363b54103beb610
Anna Raketa <rocketvsaliens@gmail.com>
2024-02-10 18:22:16 +0300

    Initial, add user model

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=20, verbose_name='телефон', **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/avatar/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ('id', )
